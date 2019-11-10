82555c731c5ae8edd14c07909b247b6a83dd63e3
debugtalk <mail@debugtalk.com>
2019-11-10 15:29:16 +0800

    fix: yaml path

--- tests/test_single_api.py
+++ tests/test_single_api.py
@@ -9,6 +9,8 @@ class TestSingleAPI(unittest.TestCase):
     def test_loader_single_api(self):
         """ 加载出的接口请求参数与原始信息一致
         """
-        single_api_yaml = os.path.join(os.getcwd(), "api", "get_homepage.yml")
+
+        single_api_yaml = os.path.join(
+            os.path.dirname(__file__), "api", "get_homepage.yml")
         loaded_json = load_yaml(single_api_yaml)
         self.assertEqual(loaded_json["url"], "https://mubu.com/")
